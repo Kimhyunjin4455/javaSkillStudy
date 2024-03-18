b2f11cad76c11af0e6713b972b3318700f2233dc
Kimhyunjin4455 <dp4455@naver.com>
2024-03-18 21:12:05 +0900

    Prepare git push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+    // merge 후, push 전 커밋 내용 변경하기 위한 주석
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
